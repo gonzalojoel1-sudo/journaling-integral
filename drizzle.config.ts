506eaf0a6bf0a93ed20c1776ded658f7de8efcf2
gonzalojoel1-sudo <gonzalojoel1@gmail.com>
2026-06-30 02:45:35 -0300

    update: subiendo nuevas modificaciones desde la compu

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+// Forzamos a la terminal a buscar y leer físicamente el archivo .env de la raíz
+dotenv.config({
+  path: '.env'
+});
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'sqlite',
+  dialect: 'turso', // Cambiado a 'turso' para que inicialice el protocolo de seguridad de internet de forma nativa
   dbCredentials: {
-    url: process.env.DATABASE_URL || 'file:local.db',
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 });
